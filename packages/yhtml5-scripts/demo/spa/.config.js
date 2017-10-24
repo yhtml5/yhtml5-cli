c0cebb2e2edc4e8729c4953d793602e2103a5a8d
张大漾 <1417376255@qq.com>
2017-10-24 22:58:07 +0800

    update: yhtml5-scripts spa demo

--- packages/yhtml5-scripts/demo/spa/.config.js
+++ packages/yhtml5-scripts/demo/spa/.config.js
@@ -3,15 +3,36 @@
  * Description: The configuration file for the yhtml5-scripts should not be packaged into the app
  *
  */
-
 const path = require('path')
 
 const config = {
+  host: '0.0.0.0',
+  devPort: 9991,
+
+
+
+  entry: 'src/index.js',
+  analyzerPort: 9992,
+  distributePort: 9993,
+  pages: [{
+    title: '测试1',
+    key: 'index',
+    filename: 'index.html',
+    path: 'src/pages/resume.js'
+  }, {
+    key: 'luyan',
+    title: '测试2',
+    filename: 'luyan.html',
+    path: 'src/pages/luyan.js'
+  }],
+  output: '/',
+  type: '',
+  templateHtml: 'src/pages/template.js',
   test: {
-    testMatch: ['demo/__test__/**/*.js?(x)'],
-    transformIgnorePatterns: ["node_modules/(?!(yhtml5-test|react-redux|react-native-button)/)"],
-    moduleNameMapper: webpackConfigAlias,
-    collectCoverageFrom: ['src/**/*.{js,jsx}'],
+    // testMatch: ['demo/__test__/**/*.js?(x)'],
+    // transformIgnorePatterns: ["node_modules/(?!(yhtml5-test|react-redux|react-native-button)/)"],
+    // moduleNameMapper: webpackConfigAlias,
+    // collectCoverageFrom: ['src/**/*.{js,jsx}'],
   }
 }
 
