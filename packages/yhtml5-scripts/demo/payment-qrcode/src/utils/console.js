3cebe2a92f9857f97e5905af46b41ff7c2786f40
张大漾 <1417376255@qq.com>
2017-11-16 11:18:42 +0800

    delete: demo/payment-qrcode/

--- packages/yhtml5-scripts/demo/payment-qrcode/src/utils/console.js
+++
@@ -1,9 +0,0 @@
-import VConsole from 'vconsole'
-
-const vConsole = new VConsole({})
-
-// console.log('1');   // 白底黑字
-// console.info('2');  // 白底紫字
-// console.debug('3');  // 白底黄字
-// console.warn('4');  // 黄底黄字
-// console.error('5'); // 红底红字
