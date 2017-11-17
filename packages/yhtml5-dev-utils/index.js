456b1615010ada9d0fdbfa50bdd839f1f54873a4
张大漾 <1417376255@qq.com>
2017-11-17 21:01:08 +0800

    update: add getVersion function

--- packages/yhtml5-dev-utils/index.js
+++ packages/yhtml5-dev-utils/index.js
@@ -1,12 +1,14 @@
 const checkRequiredFiles = require('./src/checkRequiredFiles')
 const clearConsole = require('./src/clearConsole')
-const crossSpawn = require('./src/crossSpawn.js')
-const openBrowse = require('./src/openBrowser.js')
+const crossSpawn = require('./src/crossSpawn')
+const openBrowse = require('./src/openBrowser')
+const getVersion = require('./src/getVersion')
 
 
 module.exports = {
   checkRequiredFiles,
   clearConsole,
   crossSpawn,
-  openBrowse
+  openBrowse,
+  getVersion
 }
