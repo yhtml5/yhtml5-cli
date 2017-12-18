3e9b979392686025d88dfacaf1f52f17f5ac57cb
张大漾 <1417376255@qq.com>
2017-12-18 23:30:40 +0800

    yhtml5-scripts: update notRepeat

--- packages/yhtml5-utils/src/notRepeating.js
+++ packages/yhtml5-utils/src/notRepeating.js
@@ -1,17 +1,28 @@
 let awaitStatus = true
-
 const timer = (time) => new Promise((resolve) => setTimeout(resolve, time))
 
-async function notRepeating({
-  callback = () => { },
-  time = 1000
-}) {
-  if (awaitStatus) {
-    awaitStatus = false
-    callback()
-    await timer(time)
-    awaitStatus = true
-  }
-}
+const notRepeating = (time = 1000) =>
+  new Promise((resolve, reject) => {
+    if (awaitStatus) {
+      awaitStatus = false
+      resolve()
+      timer(time)
+        .then(() => { awaitStatus = true })
+    } else {
+      reject('Do not repeat!')
+    }
+  })
+
+// async function notRepeating({
+//     callback = () => { },
+//   time = 1000
+//   }) {
+//   if (awaitStatus) {
+//     awaitStatus = false
+//     callback()
+//     await timer(time)
+//     awaitStatus = true
+//   }
+// }
 
 export default notRepeating
