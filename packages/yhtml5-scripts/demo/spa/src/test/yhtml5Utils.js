12a282082295e621281804c719e32e1b63c8618b
张大漾 <1417376255@qq.com>
2018-01-24 11:47:20 +0800

    test yhtml5-utils

--- packages/yhtml5-scripts/demo/spa/src/test/yhtml5Utils.js
+++ packages/yhtml5-scripts/demo/spa/src/test/yhtml5Utils.js
@@ -1,10 +1,6 @@
-// import utils from 'yhtml5-utils';
+import utils from 'yhtml5-utils';
 
 // utils.createScriptAsync('//cdn.bootcss.com/jquery/3.2.1/jquery.min.js')
 // .then(() => console.log('\ndone!',$))
 
-// console.log(utils)
-
-
-
-
+console.log('yhtml5-utils', utils)
