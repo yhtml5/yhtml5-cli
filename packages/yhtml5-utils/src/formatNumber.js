004b7297f2c214d14383c7765b45e1534156f8ae
张大漾 <1417376255@qq.com>
2018-06-12 10:42:26 +0800

    update formatNumber util

--- packages/yhtml5-utils/src/formatNumber.js
+++ packages/yhtml5-utils/src/formatNumber.js
@@ -8,7 +8,6 @@
  * @param {string} roundtag               舍入参数, [ceil: 向上取整, floor: 向下取整, round: 四舍五入], 默认 round
  * @param {string} decPoint               小数点符号, 默认 '.'
  * @param {string} thousandsSep           千分位符号, 默认 无
- *
  */
 
 function formatNumber(values) {
@@ -37,10 +36,8 @@ function formatNumber(values) {
     if (beforeTransform) {
       number = beforeTransform(number)
     }
-
     // let number = JSON.parse(JSON.stringify(value))
     number = (number + '').replace(/[^0-9+-Ee.]/g, '')
-
     let n = !isFinite(+number) ? 0 : +number,
       prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
       s = '',
@@ -50,41 +47,33 @@ function formatNumber(values) {
         return '' + parseFloat(Math[roundtag](parseFloat((n * k).toFixed(prec * 2))).toFixed(prec * 2)) / k;
       };
     s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-
     if (thousandsSep) {
       const re = /(-?\d+)(\d{3})/;
       while (re.test(s[0])) {
         s[0] = s[0].replace(re, "$1" + thousandsSep + "$2");
       }
     }
-
     if ((s[1] || '').length < prec) {
       s[1] = s[1] || '';
       s[1] += new Array(prec - s[1].length + 1).join('0');
     }
-
     return s.join(decPoint);
   }
 }
 
-
 // const value = 999999.715001
-
 // const a = new formatNumber({
 //     decimals: 2,
 //     decPoint: '.',
 //     thousandsSep: null,
 //     roundtag: 'round'
 // })
-
 // const b = new formatNumber({
 //     decimals: 3,
 //     decPoint: '.',
 //     roundtag: 'ceil'
 // })
-
 // const c = new formatNumber()
-
 // const d = new formatNumber({
 //     decimals: 3,
 //     decPoint: '.',
@@ -94,12 +83,11 @@ function formatNumber(values) {
 //     }
 // })
 
-
-
 // a(value)         // 999,999.72
 // b(value)         // 999999.716
 // c(value)         // 1000000
 // d(value)         // 9999.998
+// e(value)         // 100万
 
 export default formatNumber
 
